64d8d3e9046dc5858480714052b6bc95f1538ca6
gnanasekar <gnanasekar69@gmail.com>
2019-07-19 21:08:02 +0530

    Team leaf automation day1

---
+++ src/main/java/POM/TestLeaf/Pages/CreateLead.java
@@ -0,0 +1,22 @@
+package POM.TestLeaf.Pages;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CreateLead {
+
+	public static void main(String[] args)
+	{
+		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
+		ChromeDriver d = new ChromeDriver();
+		d.get("http://leaftaps.com/opentaps/control/main");
+		d.manage().window().maximize();
+		d.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		d.findElementById("username").sendKeys("demosalesmanager");
+		d.findElementById("password").sendKeys("crmsfa");
+		
+	}
+
+}
