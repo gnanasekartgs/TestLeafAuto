1d25910775ada49ecf9ebe29f6bc7b40ecbb3a12
gnanasekar <gnanasekar69@gmail.com>
2019-07-23 10:43:09 +0530

    myn

--- src/main/java/Demo/Myntra.java
+++ src/main/java/Demo/Myntra.java
@@ -1,8 +1,12 @@
 package Demo;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
@@ -20,8 +24,23 @@ public class Myntra {
 		d.findElementByXPath("(//div[@class='desktop-navLink']/a)[1]").click();
 		d.findElementByXPath("(//a[@class='navi-link  navi-underline'])[1]").click();
 		
-		Actions action = new Actions(d);
-		action.moveToElement(By.xpath("(//picture[@class='img-responsive'])[1]")))
+		
+		WebElement driver = d.findElementByXPath("//img[@alt='Moda Rapido Men Maroon Printed Round Neck T-shirt']");
+		//WebElement driver = d.findElementByXPath("(//picture[@class='img-responsive'])[1]");
+//		Actions action = new Actions(d);
+//		action.moveByOffset(210, 280).perform();
+		driver.click();
+		
+
+		Set<String> allwindow = d.getWindowHandles();
+		List<String> all =new ArrayList();
+		all.addAll(allwindow);
+		d.switchTo().window(all.get(1));
+		d.findElementByXPath("//p[text()='L']").click();
+		d.findElementByClassName("myntraweb-sprite pdp-whiteBag sprites-whiteBag pdp-flex pdp-center").click();
+		d.findElementByXPath("//span[text()='GO TO BAG']").click();
+		
+		
 		
 	}
 
